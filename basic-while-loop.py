012125a4e4fd194a824598a438efd8cfa7e46371
jackiepareja <jaxpaer@gmail.com>
2018-07-27 20:43:03 -0700

    5.2.1 challenge

---
+++ basic-while-loop.py
@@ -0,0 +1,8 @@
+#Write an expression that executes the loop body as long as the user enters a non-negative number
+
+user_num = 9;
+while user_num >= 0:
+    print('Body')
+    user_num = int(input())
+
+print('Done')
