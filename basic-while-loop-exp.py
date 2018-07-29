5ea1d51d70faacec7cbc2c180aa5096c1d43456d
jackiepareja <jaxpaer@gmail.com>
2018-07-29 13:49:08 -0700

    insect growth challenge

---
+++ basic-while-loop-exp.py
@@ -0,0 +1,7 @@
+#Write a while loop that prints user_num divided by 2 until user_num is less than 1.
+
+
+user_num = 20;
+while user_num >= 1:
+    print(user_num)
+    user_num = user_num/2
---
+++ while_loop_insect_growth.py
@@ -0,0 +1,10 @@
+#Given positive integer num_insects,
+#write a while loop that prints that number doubled without exceeding 100.
+#Follow each number with a space.
+#Ex: if num_insects = 8, print:
+# 8, 16, 32, 64
+
+num_insects = 8
+
+while num_insects < 100:
+    print(num_insects)
